6a3db5bce92d510f9010b13d16f5b671d31aaf49
Michalis Antoniou <MAntoniou@CircleLinkHealth.com>
2016-12-11 21:01:02 -0500

    Adds filters and status checkboxes

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,14 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
   handleSubmit(event) {
     event.preventDefault();
  
@@ -22,7 +30,7 @@ class App extends Component {
     // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
-  
+
   getTasks() {
     return [
       { _id: 1, text: 'This is task 1' },
@@ -31,8 +39,18 @@ class App extends Component {
     ];
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -42,6 +60,17 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
               type="text"
@@ -65,6 +94,6 @@ App.propTypes = {
  
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
